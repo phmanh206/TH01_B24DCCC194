3673be55eda20eff5ae96ae490d7e7fad3a9870f
Manh <phammanh123xyz@gmail.com>
2025-09-10 09:22:08 +0700

    fff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+function DisplayNumber({value}) {
+  return <h2>Giá trị hiện tại: {value}</h2>;
+}
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 20}}>
+      <DisplayNumber value={count} />
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
 
-export default App;
+export default CounterApp;
